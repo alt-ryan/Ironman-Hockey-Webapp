fdcbc7e17da0a572cfdb43bbf2a67536460bde04
Ryan McKay <rmckay9357@gmail.com>
2020-02-21 15:44:52 -0500

    Refactoring a bit. Adding boostrap/simple js. Dynamically getting top active player

--- app.js
+++ app.js
@@ -3,37 +3,65 @@ const app = express();
 const request = require("request");
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 var data = [];
+var url = "";
+var activeStreakLeader = "";
+var activeStreakLeaderID = "";
+var activeStreak = 0;
 
+
+// Home page
 app.get("/", (req, res) => {
-    let url = "https://statsapi.web.nhl.com/api/v1/people/8471735/stats?stats=careerRegularSeason";
 
-    request(url, (err, response, body) => {
-		if(!err && response.statusCode == 200) {
-            var yandle = JSON.parse(body);
-            data.push(yandle.stats[0].splits[0].stat.games);
-			//res.render("home", {data: data});
-        }
-        else {
-            res.render("errorPage");
-        }
-    });
-
+    // Current list of longest streaks (active and inactive)
     url = "https://records.nhl.com/site/api/games-played-streak-skaters?sort=streak&dir=DESC";
 
     request(url, (err, response, body) => {
 		if(!err && response.statusCode == 200) {
-            var recordList = JSON.parse(body);
-            data.push(recordList.data);
-			res.render("home", {data: data});
+            let recordList = JSON.parse(body);
+            data.push(recordList.data.slice(0,10));
+
+            // Get the active streak leader
+            recordList.data.some((player, index, _arr) => {
+                if (player.activePlayer === true){
+                    activeStreakLeader = player.firstName + " " + player.lastName;
+                    activeStreak = player.streak;
+                    activeStreakLeaderID = player.playerId;
+
+                    // Update url to search for the current active streak leader
+                    url = "https://statsapi.web.nhl.com/api/v1/people/" + activeStreakLeaderID + "/stats?stats=careerRegularSeason";
+                    
+                    return activeStreakLeader !== "";
+                }
+            });
         }
         else {
             res.render("errorPage");
         }
-    });
-})
 
+        //todo: refactor this for better asynchronous handling
+        //      nesting requests isn't best practice. doing it for now for working build
+        request(url, (err, response, body) => {
+            if(!err && response.statusCode == 200) {
+                let player = JSON.parse(body);
+                data.push(player.stats[0].splits[0].stat.points);
+                //res.render("home", {data: data});
+                res.render("home", {
+                    data: data, 
+                    activeStreakLeader: activeStreakLeader,
+                    activeStreak: activeStreak
+                });
+            }
+            else {
+                res.render("errorPage");
+            }
+        });
+    });
+});
+
+// Start the server
 app.listen(3000,() => {
 	console.log("Listening on port 3000");
 });
