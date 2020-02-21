18ac7393bcdcbbe33d450e35fdffc3d62bab588c
Ryan McKay <rmckay9357@gmail.com>
2020-02-20 20:03:02 -0500

    Add first API requests. List the streak record holders.

--- app.js
+++ app.js
@@ -4,12 +4,28 @@ const request = require("request");
 
 app.set("view engine", "ejs");
 
+var data = [];
+
 app.get("/", (req, res) => {
     let url = "https://statsapi.web.nhl.com/api/v1/people/8471735/stats?stats=careerRegularSeason";
 
     request(url, (err, response, body) => {
 		if(!err && response.statusCode == 200) {
-			var data = JSON.parse(body);
+            var yandle = JSON.parse(body);
+            data.push(yandle.stats[0].splits[0].stat.games);
+			//res.render("home", {data: data});
+        }
+        else {
+            res.render("errorPage");
+        }
+    });
+
+    url = "https://records.nhl.com/site/api/games-played-streak-skaters?sort=streak&dir=DESC";
+
+    request(url, (err, response, body) => {
+		if(!err && response.statusCode == 200) {
+            var recordList = JSON.parse(body);
+            data.push(recordList.data);
 			res.render("home", {data: data});
         }
         else {
